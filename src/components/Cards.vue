d28aa95f1a6452e460ddbe12afa380b5e3befb8a
cleisonsantos <css.cleison@outlook.com>
2019-02-04 21:52:12 -0200

    Cards exibindo os preços e os ingredientes de cada açaí.

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <v-container v-for="sabor in sabores" :key="sabor.title">
+    <v-container v-for="(sabor,key) in sabores" :key="key">
       <v-card hover>
         <v-container>
-          <v-img :src="sabor.img" height="200px">
+          <v-img :src="sabor.src" height="200px">
             <v-container fill-height fluid pa-2>
               <v-layout fill-height>
                 <v-flex xs12 align-end flexbox>
@@ -23,7 +23,25 @@
             <v-btn icon>
               <v-icon>share</v-icon>
             </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn :key="key" icon @click="show = !show">
+              <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+            </v-btn>
           </v-card-actions>
+          <v-slide-y-transition>
+            <v-card-text v-show="show">
+              <div class="acai-info">
+                  <li :key="key" v-for="(ingrediente,key) in sabor.ingredientes">{{ ingrediente }}</li>
+                </div>
+                <hr >
+                <div class="acai-info">
+                  <li
+                    :key="key"
+                    v-for="(valor,key) in sabor.valores"
+                  >{{ key + "ml" }} - {{ "R$ " + valor.toFixed(2).replace(".",",") }}</li>
+                </div>
+            </v-card-text>
+          </v-slide-y-transition>
         </v-container>
       </v-card>
     </v-container>
@@ -35,10 +53,18 @@ export default {
   name: "Cards",
   data() {
     return {
-      sabores: []
+      sabores: [],
+      show: false
     };
   },
-  methods: {},
+  methods: {
+    listIngredients() {
+      sabor.ingredientes.forEach(element => {
+        console.log(element);
+        return element.values.toString;
+      });
+    }
+  },
   created() {
     this.$http.get("sabores.json").then(res => {
       this.sabores = res.data;
@@ -48,5 +74,10 @@ export default {
 };
 </script>
 
-<style>
+<style >
+.acai-info div {
+}
+.acai-info li {
+  list-style: none;
+}
 </style>
